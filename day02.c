16ad285d76056511308e304eae651ff093eb0b02
Ratnesha Gupta <RATNESHA.23288@stu.upes.ac.in>
2025-10-06 10:16:25 +0530

    Update day02.c

--- day02.c
+++ day02.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main() {
+    float length, breadth, area, perimeter;
+
+    printf("Enter length of rectangle: ");
+    scanf("%f", &length);
+
+    printf("Enter breadth of rectangle: ");
+    scanf("%f", &breadth);
+ 
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    printf("Area of rectangle = %.2f\n", area);
+    printf("Perimeter of rectangle = %.2f\n", perimeter);
+
+    return 0;
+}
+
+
+#include <stdio.h>
+#define PI 3.14159   
+
+int main() {
+    float radius, area, circumference;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = PI * radius * radius;
+    circumference = 2 * PI * radius;
+    
+    printf("Area of circle = %.2f\n", area);
+    printf("Circumference of circle = %.2f\n", circumference);
+
+    return 0;
+}
