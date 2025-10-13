6bfae97a486905fbb20c22a90dfaca0d514b975a
Ratnesha Gupta <RATNESHA.23288@stu.upes.ac.in>
2025-10-13 14:59:39 +0530

    Add files via upload

---
+++ day26_b.c
@@ -0,0 +1,22 @@
+//Write a program to print the following pattern:
+
+#include <stdio.h>
+
+int main() {
+    // First group: 1 star
+    printf("*\n\n");
+
+    // Second group: 3 stars
+    printf("*\n*\n*\n\n");
+
+    // Third group: 5 stars
+    printf("*\n*\n*\n*\n*\n\n");
+
+    // Fourth group: 3 stars
+    printf("*\n*\n*\n\n");
+
+    // Fifth group: 1 star
+    printf("*\n");
+
+    return 0;
+}
