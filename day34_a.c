14cbbe60883c9531459f04704192b17ed50d404a
Ratnesha Gupta <RATNESHA.23288@stu.upes.ac.in>
2025-10-24 22:04:31 +0530

    Add files via upload

---
+++ day34_a.c
@@ -0,0 +1,47 @@
+//Insert an element in an array at a given position.
+
+#include <stdio.h>
+
+int main() {
+    int n, i, pos, value;
+
+  
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[100];  
+
+   
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+   
+    printf("Enter the position to insert (1 to %d): ", n + 1);
+    scanf("%d", &pos);
+    printf("Enter the element to insert: ");
+    scanf("%d", &value);
+
+    
+    if(pos < 1 || pos > n + 1) {
+        printf("Invalid position! 😕\n");
+    } else {
+        
+        for(i = n; i >= pos; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+    arr[pos - 1] = value;
+        n++; 
+
+        
+        printf("Array after insertion:\n");
+        for(i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
